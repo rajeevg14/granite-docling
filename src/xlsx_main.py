3dd0d7f5d44cae92022d983aa7ba128eae964025
rajeevg14 <rkssgupta@gmail.com>
2025-04-05 16:49:00 -0400

    updated

---
+++ src/xlsx_main.py
@@ -0,0 +1,18 @@
+from docling.document_converter import DocumentConverter
+import pandas as pd
+
+pdf_source = "data/pdf/2309.13057v3.pdf"
+converter = DocumentConverter()
+pdf_result = converter.convert(pdf_source)
+md = pdf_result.document.export_to_markdown()
+
+file_name = "data/xlsx/test-01.xlsx"
+sheets = pd.read_excel(file_name, sheet_name=None)
+for sheet in sheets:
+    df = pd.read_excel(file_name, sheet_name=sheet)
+    clms = df.columns.to_list()
+    for clm in clms:
+        if clm =="Name":
+            word = df.loc[0, clm]
+            if word in md: 
+                print('success')
